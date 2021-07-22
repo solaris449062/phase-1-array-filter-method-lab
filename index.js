377387f816312ca097cb428f942dc0dcdda26105
Sungoh Park <spark449062@gmail.com>
2021-07-22 15:07:00 -0600

    commit tree 3ae32e6b11c4ec0f57ab06dbd8045a924dd42aab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(drivers, inputDriverName) {
+    return drivers.filter(driver => driver.toUpperCase() === inputDriverName.toUpperCase());
+}
+
+function fuzzyMatch(drivers, fuzzyName) {
+    function testMatch(element) {
+        return element.startsWith(fuzzyName);
+    }
+    return drivers.filter(testMatch);
+}
+
+function matchName(driver, name) {
+    function testMatch(element) {
+        return element.name == name
+    }
+    return driver.filter(testMatch)
+}
+const driversList = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+// console.log("Bobby".toUpperCase);
+// console.log(findMatching(driversList, "Bobby"));
+console.log(fuzzyMatch(driversList, "r"))
